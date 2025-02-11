1da3bbd692d48f5972498fefe871aacffd5d17bc
Data-Git-Hub <s576977@nwmissouri.edu>
2025-02-11 11:28:51 -0600

    V 8.5 - Corrected error in update_records.sql

--- sql_features/update_records.sql
+++ sql_features/update_records.sql
@@ -2,10 +2,10 @@
 -- Update Harper Lee's last name by appending " (Updated)"
 UPDATE authors
 SET last = last || ' (Updated)'
-WHERE author_id = '10f88232-1ae7-4d88-a6a2-dfcebb22a596';
+WHERE author_id = 'AUTHOR_003';
 
 -- Update the title of "1984" to indicate a revised edition
 UPDATE books
 SET title = '1984 (Revised Edition)'
-WHERE book_id = '0f5f44f7-44d8-4f49-b8c4-c64d847587d3';
+WHERE book_id = 'BOOK_003';
 
